1ab5b650e879d925f35f0d265dc080b6f4395750
pdkokare1 <pdkokare1@gmail.com>
2026-03-20 16:58:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchCategories(); 
     fetchBrands();
     fetchDistributors();
+    fetchPromotions(); // NEW: Added for Phase 2 to load discount rules on startup
     fetchOrders();
     renderOverview(); 
 });
